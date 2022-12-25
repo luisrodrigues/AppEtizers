a9ad1428e02a8028cdd122083d55ede63388aa6c
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-25 19:54:53 +0000

    Initial Commit

---
+++ AppEtizers/AppEtizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEtizersApp.swift
+//  AppEtizers
+//
+//  Created by Rodrigues, Luis (EXTERN: EEDS) on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppEtizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
