6025b5df14753708f8ad57688fd269c42a2e7942
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-26 09:24:06 +0000

    Added appetiser model and sample image

---
+++ AppEtizers/Appetiser.swift
@@ -0,0 +1,38 @@
+//
+//  Appetiser.swift
+//  AppEtizers
+//
+//  Created by Rodrigues, Luis on 26/12/2022.
+//
+
+import Foundation
+
+struct Appetiser: Decodable {
+	let id: Int
+	let name: String
+	let description: String
+	let price: Double
+	let imageURL: String
+	let calories: Int
+	let protein: Int
+	let carbs: Int
+}
+
+struct AppetiserResponse {
+	let request: [Appetiser]
+}
+
+struct MockData {
+	static let sampleAppetiser = Appetiser(
+		id: 0001,
+		name: "Test Apple",
+		description: "Test",
+		price: 11.99,
+		imageURL: "",
+		calories: 1,
+		protein: 1,
+		carbs: 1
+	)
+	
+	static let appetisers = [sampleAppetiser, sampleAppetiser, sampleAppetiser, sampleAppetiser]
+}
